a8964c64e8882857ec0d9f57d64d9cfc16c2bda7
alexRhine22 <alextherhine@gmail.com>
2024-04-18 16:53:49 -0400

    Fixed bug where mobile nav would stay open on page resize

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ const headerNav = document.getElementById('header-nav');
 
 currentYearElement.textContent = new Date().getFullYear();
 
+const removeMobileNavActiveStyles = () => {
+  headerNav.classList.remove('active');
+  document.body.style.overflow = 'auto';
+  mobileMenuButton.setAttribute('aria-expanded', 'false');
+  header.classList.remove('active');
+};
+
+const debounce = (func, timeout = 300) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+};
+
+const handleTabletChange = () => {
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+  if (mediaQuery.matches) {
+    console.log('Media Query Matched!');
+    removeMobileNavActiveStyles();
+  }
+};
+
+const debouncedHandleTabletChange = debounce(handleTabletChange);
+
 mobileMenuButton.addEventListener('click', () => {
   if (header.classList.contains('active')) {
     headerNav.classList.remove('active');
@@ -13,9 +41,8 @@ mobileMenuButton.addEventListener('click', () => {
     mobileMenuButton.setAttribute('aria-expanded', 'false');
     header.classList.remove('active');
   } else {
-    document.body.style.overflow = 'hidden';
-    header.classList.add('active');
-    headerNav.classList.add('active');
-    mobileMenuButton.setAttribute('aria-expanded', 'true');
+    removeMobileNavActiveStyles();
   }
 });
+
+window.addEventListener('resize', debouncedHandleTabletChange);
